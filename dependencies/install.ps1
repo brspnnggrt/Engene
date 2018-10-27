9fe5403d71815f6ce4b649811d0d758fb5441393
Braspenning Gert <braspge@cronos.be>
2018-10-27 13:31:28 +0200

    Add install script for dependencies mingw and msys

---
+++ dependencies/install.ps1
@@ -0,0 +1,12 @@
+# Uncomment if needed!
+
+# install chocolatey
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# choco feature enable -n=allowGlobalConfirmation
+
+# IDE
+# choco install vscode
+
+# C++ dev tooling
+choco install mingw # c++ dev tools for windows
+choco install msys2 # c++ dev tools for windows
