12ae343c0363281eba14fbb208d0d872e42110b8
Braspenning Gert <braspge@cronos.be>
2018-10-27 14:43:30 +0200

    Add vec3

---
+++ src/math/vec3.cpp
@@ -0,0 +1,90 @@
+#include "vec3.h"
+
+namespace engene { namespace math {
+
+	vec3::vec3() {
+		x = 0.0f;
+		y = 0.0f;
+		z = 0.0f;
+	}
+
+	vec3::vec3(const float& _x, const float& _y, const float& _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	vec3& vec3::add(const vec3& other) {
+		x += other.x;
+		y += other.y;
+		z += other.z;
+		return *this;
+	}
+
+	vec3& vec3::subscract(const vec3& other) {
+		x -= other.x;
+		y -= other.y;
+		z -= other.z;
+		return *this;
+	}
+
+	vec3& vec3::multiply(const vec3& other) {
+		x *= other.x;
+		y *= other.y;
+		z *= other.z;
+		return *this;
+	}
+
+	vec3& vec3::divide(const vec3& other) {
+		x /= other.x;
+		y /= other.y;
+		z /= other.z;
+		return *this;
+	}
+
+	vec3 operator+(vec3 left, const vec3& right) {
+		return left.add(right);
+	}
+
+	vec3 operator-(vec3 left, const vec3& right) {
+		return left.subscract(right);
+	}
+
+	vec3 operator*(vec3 left, const vec3& right) {
+		return left.multiply(right);
+	}
+
+	vec3 operator/(vec3 left, const vec3& right) {
+		return left.divide(right);
+	}
+
+	bool vec3::operator==(vec3& other) {
+		return other.x == x && other.y == y && other.z == z;
+	}
+
+	bool vec3::operator!=(vec3& other) {
+		return !(*this == other);
+	}
+
+	vec3& vec3::operator+=(const vec3& other) {
+		return add(other);
+	}
+
+	vec3& vec3::operator-=(const vec3& other) {
+		return subscract(other);
+	}
+
+	vec3& vec3::operator*=(const vec3& other) {
+		return multiply(other);
+	}
+
+	vec3& vec3::operator/=(const vec3& other) {
+		return divide(other);
+	}
+
+	std::ostream& operator<<(std::ostream& stream, const vec3& vector) {
+		stream << "vec3: (" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+		return stream;
+	}
+
+} }
